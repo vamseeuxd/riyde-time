5f5afaa03225997cf7e27691fac44f45cd28a539
vamseeuxd <vamsee.uxd@gmail.com>
2022-12-22 11:02:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# riyde-time
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-2v167d)
---
+++ src/app/logo/logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  templateUrl: './logo.component.html',
+  styleUrls: ['./logo.component.css']
+})
+export class LOGOComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
