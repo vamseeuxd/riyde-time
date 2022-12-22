b511b23289da9737de6f607c19d57fa273123d81
vamseeuxd <vamsee.uxd@gmail.com>
2022-12-22 19:32:29 +0530

    2 files modified, 1 added

--- src/app/manage-cars/manage-cars.component.ts
+++ src/app/manage-cars/manage-cars.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-manage-cars',
   templateUrl: './manage-cars.component.html',
-  styleUrls: ['./manage-cars.component.css']
+  styleUrls: ['./manage-cars.component.css'],
 })
 export class ManageCarsComponent implements OnInit {
+  cars = [
+    { name: 'Car-01', id: '01' },
+    { name: 'Car-02', id: '02' },
+    { name: 'Car-03', id: '03' },
+    { name: 'Car-04', id: '04' },
+    { name: 'Car-05', id: '05' },
+    { name: 'Car-06', id: '06' },
+  ];
 
-  constructor() { }
+  isEdit = false;
 
-  ngOnInit() {
+  closeResult: string;
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
+  open(content, isEdit = false) {
+    this.isEdit = isEdit;
+    this.modalService.open(content);
+  }
+
+  ngOnInit() {}
 }
