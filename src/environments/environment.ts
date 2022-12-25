adee7dfa25b7ec642d0e1bf9e7204c65bef8637c
vamseeuxd <vamsee.uxd@gmail.com>
2022-12-25 13:05:35 +0530

    2 files added

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBDQh0H3zNa5rObmjdPrzK4CZADgaZ9gPo',
+    authDomain: 'riydetime.firebaseapp.com',
+    projectId: 'riydetime',
+    storageBucket: 'riydetime.appspot.com',
+    messagingSenderId: '483054332736',
+    appId: '1:483054332736:web:7921bff30bca7fea76a8a5',
+    measurementId: 'G-FF34MHB47E',
+  },
+};
